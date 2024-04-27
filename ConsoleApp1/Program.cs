3be69f3fe5ab31e5ab8cdb02e45fea939d406b13
Aboodzughr <abedamjadz2003@icloud.com>
2024-04-27 22:46:47 +0300

    h

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public enum TaskStatus
+    {
+        Todo,
+        Doing,
+        Done
+    }
+
+    public class TaskManager
+    {
+        private List<Task> tasks = new List<Task>();
+
+        public void AddTask(string description)
+        {
+            tasks.Add(new Task(description, TaskStatus.Todo));
+        }
+
+        public void ViewTasks()
+        {
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. - {tasks[i].Status}");
+            }
+        }
+
+        public void MarkTaskDone(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+        }
+
+        public void UpdateTaskStatus(int index, TaskStatus newStatus)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks[index].Status = newStatus;
+            }
+        }
+
+        public bool IsValidIndex(int index)
+        {
+            return index >= 0 && index < tasks.Count;
+        }
+    }
+
     class Program
     {
-        static List<(string, string)> tasks = new List<(string, string)>();
+        static TaskManager taskManager = new TaskManager();
 
         static void Main()
         {
@@ -21,49 +65,42 @@ namespace ConsoleApp1
                 Console.WriteLine("4. Status");
                 Console.WriteLine("5. Exit");
                 Console.WriteLine("-------------");
-                string choise = Console.ReadLine();
+                string choice = Console.ReadLine();
 
-                switch (choise)
+                switch (choice)
                 {
                     case "1":
                         Console.Write("Task: ");
-                        tasks.Add((Console.ReadLine(), "todo"));
+                        taskManager.AddTask(Console.ReadLine());
                         break;
 
                     case "2":
-                        for (int i = 0; i < tasks.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {tasks[i].Item1} - {tasks[i].Item2}");
-                        }
-
+                        taskManager.ViewTasks();
                         break;
 
                     case "3":
-                        Console.Write("Done: ");
+                        Console.Write("Done : ");
                         int doneIndex = int.Parse(Console.ReadLine()) - 1;
-                        if (doneIndex >= 0 && doneIndex < tasks.Count)
+                        if (taskManager.IsValidIndex(doneIndex))
                         {
-                            tasks.RemoveAt(doneIndex);
+                            taskManager.MarkTaskDone(doneIndex);
                         }
                         break;
 
                     case "4":
-                        Console.Write("Task: ");
+                        Console.Write("Task : ");
                         int statusIndex = int.Parse(Console.ReadLine()) - 1;
-                        if (statusIndex >= 0 && statusIndex < tasks.Count)
+                        if (taskManager.IsValidIndex(statusIndex))
                         {
-                            Console.Write("Status (todo, doing, done): ");
-                            string newStatus = Console.ReadLine().ToLower();
-
-                            if (newStatus == "todo" || newStatus == "doing" || newStatus == "done")
+                            Console.Write("Status (Todo, Doing, Done): ");
+                            if (Enum.TryParse<TaskStatus>(Console.ReadLine(), true, out TaskStatus newStatus))
                             {
-                                tasks[statusIndex] = (tasks[statusIndex].Item1, newStatus);
+                                taskManager.UpdateTaskStatus(statusIndex, newStatus);
                             }
                             else
                             {
                                 Console.WriteLine("Invalid status!");
                             }
-
                         }
                         else
                         {
